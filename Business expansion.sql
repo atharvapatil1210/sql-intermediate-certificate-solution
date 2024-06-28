16b5cfa781bdc9263b933ca8a2f68b7f5533b372
dev_atharva <145995913+atharvapatil1210@users.noreply.github.com>
2024-06-28 15:41:59 +0530

    Update Business expansion.sql
    update on customer id

--- Business expansion.sql
+++ Business expansion.sql
@@ -1,7 +1,13 @@
-SELECT ua.id, ua.first_name, ua.last_name, c.id, c.customer_name, COUNT(*)
-FROM contact con
-INNER JOIN customer c ON c.id=con.customer_id
-INNER JOIN user_account ua ON ua.id=con.user_account_id
-GROUP BY ua.id, ua.first_name, ua.last_name, c.id, c.customer_name
-HAVING COUNT(*)>1
-ORDER BY ua.id
+SELECT
+    c.customer_name,
+    CAST(SUM(i.total_price) AS DECIMAL(9,6)) AS total
+FROM
+    customer c
+INNER JOIN
+    invoice i ON c.id = i.customer_id
+GROUP BY
+    c.customer_name
+HAVING
+    SUM(i.total_price) <= 0.25 * (SELECT AVG(total_price) FROM invoice)
+ORDER BY
+    total DESC;
